3f9b76d6df83bd47ed786f72e5ee3588e5756d4d
kazuaki <kazuaki@archlinux.localdomain>
2025-09-18 21:34:06 +0530

    function in python

---
+++ function_in_python.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def solution(a, b):
+    return (a*b)/(a+b)
+
+
+def is_even(num):
+    pass
+
+
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+print(add(num1, num2))
+print(solution(num1, num2))
