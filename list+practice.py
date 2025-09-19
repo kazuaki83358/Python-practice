678a043210f5ea477d7e13fa6d1d216216b06f0c
kazuaki <kazuaki@archlinux.localdomain>
2025-09-19 11:25:37 +0530

    list practice

---
+++ list+practice.py
@@ -0,0 +1,16 @@
+list = [1, 2, 3, 7, 9, 1, 5, 13,
+        "nishant", True, False, 1.5]
+print(list)
+print(list[0])
+print(list[8])
+list.append(10)
+print(list)
+list.remove(1)
+print(list)
+list.pop()
+print(list)
+list2 = [10, 12, 3, 4, 15, 6, 7, 18, 91, 10]
+list2.sort()
+print(list2)
+list.clear()
+print(list)
