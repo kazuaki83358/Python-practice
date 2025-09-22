8e51b1ef5c2fbb90b31dc837296afd46373c36af
kazuaki <kazuaki@archlinux.localdomain>
2025-09-22 20:29:14 +0530

    creating file using read and write function and creating folder using os

--- os_read_write_in.py
+++ os_read_write_in.py
@@ -1,7 +1,12 @@
 import os
 
+# It will print current working direcotry 
+print("Current directory: ", os.getcwd())
+# It will create .txt file if do not exist and write in it
 file_path = "try.txt"
 with open(file_path, 'w') as f:
     f.write("hello this is my first txt file writing using python")
 
 os.mkdir('Api_calling')
+# It will list all the files in current directory 
+print(os.listdir("."))
